ac72f53f8ebe74cc77a607f79319b62b01ae5abc
camek <camekommd@gmail.com>
2023-06-15 04:27:55 -0600

    1.2 update

--- Assets/LengthyLockdownScripts.cs
+++ Assets/LengthyLockdownScripts.cs
@@ -11,6 +11,7 @@ using Math = ExMath;
 public class LengthyLockdownScripts : MonoBehaviour {
 
    public KMBombInfo Bomb;
+   public KMBombInfo BombInfo;
    public KMAudio Audio;
    public KMBombModule Module;
 
@@ -20,7 +21,11 @@ public class LengthyLockdownScripts : MonoBehaviour {
    public Color solveColor = new Color(63f / 255f, 255f / 255f, 15f / 255f);
    public Color logColor = new Color(253f / 255f, 235f / 255f, 1f / 255f);
 
-   int currentStage = 0;
+   float realTime = 0; //realTime of the final log
+   int currentStage = 0; //stage of integers
+   int correctTime = 0; //bomb time
+   int realTimeCalc;
+   public List<int> excludedNumbers = new List<int> { 12, 5, 14, 7, 20, 8 }; //exclude these
    string[] nums = {"12", "5", "14", "7", "20", "8"}; //up to 7 cases
    string queryEntireLog = "125147208"; //all cases of the positive results
    string currentLog = ""; //what your current log is
@@ -28,6 +33,18 @@ public class LengthyLockdownScripts : MonoBehaviour {
    static int ModuleIdCounter = 1;
    int ModuleId;
    private bool ModuleSolved;
+
+   int GenerateRandomNumber()
+       {
+           int randomNumber;
+
+           do
+           {
+               randomNumber = UnityEngine.Random.Range(1, 21);
+           } while (excludedNumbers.Contains(randomNumber));
+
+           return randomNumber;
+       }
 
    void Awake () {
       ModuleId = ModuleIdCounter++;
@@ -54,9 +71,8 @@ public class LengthyLockdownScripts : MonoBehaviour {
       Buttons[2].AddInteractionPunch();
    }
 
-   void PressQuery(){
+   void PressQuery(){ //Query is the log button. For some reason, I decided to be a fucking dunce and call it the Query button before I actually made the rest of the module.
      Audio.PlaySoundAtTransform("Query", Buttons[1].transform);
-     Debug.Log("Query entered.");
      switch (currentStage)
         {
         case 9:
@@ -67,7 +83,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
             currentLog = currentLog + "8";
         }
         DisplayTexts[4].text = currentLog;
-        Debug.Log(currentLog);
         StartCoroutine("ActualQueryWait");
         break;
         case 8:
@@ -81,7 +96,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
         else{
             currentLog = currentLog + "20";
         }
-        Debug.Log(currentLog);
         DisplayTexts[4].text = currentLog;
             break;
         case 6:
@@ -95,7 +109,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
         else{
             currentLog = currentLog + "7";
         }
-        Debug.Log(currentLog);
         DisplayTexts[4].text = currentLog;
             break;
         case 4:
@@ -105,7 +118,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
         else{
             currentLog = currentLog + "14";
         }
-        Debug.Log(currentLog);
         DisplayTexts[4].text = currentLog;
             break;
         case 3:
@@ -115,7 +127,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
         else{
             currentLog = currentLog + "5";
         }
-        Debug.Log(currentLog);
         DisplayTexts[4].text = currentLog;
           break;
       case 2:
@@ -129,7 +140,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
         else{
             currentLog = currentLog + "12";
         }
-        Debug.Log(currentLog);
         DisplayTexts[4].text = currentLog;
             break;
         default:
@@ -140,28 +150,27 @@ public class LengthyLockdownScripts : MonoBehaviour {
         }
             break;
         }
+        Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Log was pressed. Current log reads: " + currentLog + ".", ModuleId);
 }
 
    void PressAccept(){
      Audio.PlaySoundAtTransform("ButtonPress", Buttons[0].transform);
-     Debug.Log("Accept registered.");
-     Debug.Log("Current stage is: " + currentStage);
+     Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Current stage is: " + currentStage + ".", ModuleId);
      switch (currentStage)
         {
         case 8:
         currentStage = currentStage + 1;
-        DisplayTexts[0].text = "FINAL LOG";
+        DisplayTexts[0].text = "8";
         break;
         case 7:
-        //if number is 11 and accepted (which is wrong)
+        //if number is random and accepted (which is wrong)
             Audio.PlaySoundAtTransform("Strike", Buttons[1].transform);
             Module.HandleStrike();
             break;
         case 6:
         currentStage = currentStage + 1;
-        int randomNumberThird = UnityEngine.Random.Range(26, 40);
-        string randomStringThird = randomNumberThird.ToString();
-        DisplayTexts[0].text = randomStringThird;
+        string randomNumberThirdString = GenerateRandomNumber().ToString();
+        DisplayTexts[0].text = randomNumberThirdString;
           break;
         case 5:
         //if number is random and accepted (which is wrong)
@@ -170,9 +179,8 @@ public class LengthyLockdownScripts : MonoBehaviour {
             break;
         case 4:
           currentStage = currentStage + 1;
-          int randomNumberSecond = UnityEngine.Random.Range(26, 40);
-          string randomStringSecond = randomNumberSecond.ToString();
-          DisplayTexts[0].text = randomStringSecond;
+          string randomNumberSecondString = GenerateRandomNumber().ToString();
+          DisplayTexts[0].text = randomNumberSecondString;
             break;
         case 3:
           currentStage = currentStage + 1;
@@ -189,17 +197,15 @@ public class LengthyLockdownScripts : MonoBehaviour {
             break;
         default:
             currentStage = currentStage + 1;
-            int randomNumber = UnityEngine.Random.Range(26, 40);
-            string randomString = randomNumber.ToString();
-            DisplayTexts[0].text = randomString;
+            string randomNumberString = GenerateRandomNumber().ToString();
+            DisplayTexts[0].text = randomNumberString;
             break;
         }
     }
 
    void PressRefuse(){
      Audio.PlaySoundAtTransform("ButtonPress", Buttons[1].transform);
-     Debug.Log("Refuse registered.");
-     Debug.Log("Current stage is: " + currentStage);
+     Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Current stage is: " + currentStage + ".", ModuleId);
      switch (currentStage)
         {
           case 9:
@@ -264,22 +270,36 @@ public class LengthyLockdownScripts : MonoBehaviour {
      yield return new WaitForSeconds(2f);
    }
 
+   void DetermineCorrectTime()
+   {
+       if (Bomb.GetSerialNumberLetters().Any(x => x.EqualsAny('A', 'E', 'I', 'O', 'U')))
+           correctTime = 7;
+       else if (Bomb.GetBatteryCount() == 2)
+           correctTime = 2;
+       else
+           correctTime = 0;
+   }
+
    //for fixes
    private IEnumerator ActualQueryWait () {
-    Debug.Log("Received final log...");
-    DisplayTexts[0].text = "COMPARING...";
+    realTime = Bomb.GetTime();
+    Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Final log was submitted at:  " + realTime.ToString() + ".", ModuleId);
+    DisplayTexts[0].text = "COMPARING";
+    DisplayTexts[4].text = "";
      Audio.PlaySoundAtTransform("QueryWait", Buttons[2].transform);
-     yield return new WaitForSeconds(7f);
+     yield return new WaitForSeconds(4f);
      OnQueryAwait();
    }
 
    void OnQueryAwait () { //Shit you need to do when you do the thing
-     if (currentLog.Equals(queryEntireLog)) {
-       Audio.PlaySoundAtTransform("Solve", Buttons[1].transform);
-       DisplayTexts[0].text = "COMPLETE";
-       DisplayTexts[0].color = solveColor;
-       DisplayTexts[4].text = "";
-       Solve();
+     //Dogshit workaround
+    realTimeCalc = (int) realTime % 10;
+     if (currentLog.Equals(queryEntireLog) & realTimeCalc == correctTime) {
+         Audio.PlaySoundAtTransform("Solve", Buttons[1].transform);
+         DisplayTexts[0].text = "SUCCESS";
+         DisplayTexts[0].color = solveColor;
+         DisplayTexts[4].text = "";
+         Solve();
      }
      else {
        currentLog = "";
@@ -288,7 +308,6 @@ public class LengthyLockdownScripts : MonoBehaviour {
        Module.HandleStrike();
        DisplayTexts[0].color = Color.red;
        StartCoroutine("Start");
-       //6 strikes for extra measure...
      }
    }
 
@@ -297,10 +316,13 @@ public class LengthyLockdownScripts : MonoBehaviour {
    }
 
    private IEnumerator Start () { //Shit
-     DisplayTexts[0].text = "FETCHING...";
+     DetermineCorrectTime(); //will rerun everytime a restart is performed - not that big of a deal... I think...
+     Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Correct time to submit final log is: " + correctTime.ToString() + ".", ModuleId);
+     DisplayTexts[0].text = "..."; // Loading text
      yield return new WaitForSeconds(2f);
      DisplayTexts[0].color = Color.white;
      DisplayTexts[0].text = nums[0];
+     DisplayTexts[4].text = "";
    }
 
    void Update () { //Shit that happens at any point after initialization
@@ -316,18 +338,94 @@ public class LengthyLockdownScripts : MonoBehaviour {
    }
 
 #pragma warning disable 414
-   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+   private readonly string TwitchHelpMessage = @"Use !{0} accept to accept a number. Use !{0} refuse to refuse a number. Use !{0} log to add a number to the log. Use !{0} log at [7,2,0] to log the last number at the correct time.";
 #pragma warning restore 414
 
    IEnumerator ProcessTwitchCommand (string Command) {
+     Command = Command.ToUpper();
       yield return null;
+
+      switch (Command) {
+        case "ACCEPT":
+          Buttons[0].OnInteract();
+          break;
+        case "REFUSE":
+          Buttons[1].OnInteract();
+          break;
+        case "LOG":
+          Buttons[2].OnInteract();
+          break;
+
+        case "LOG AT 7":
+          while ((int)BombInfo.GetTime() % 10 == correctTime)
+            yield return "trycancel";  // Fixes a really obscure bug with tp
+          while ((int)BombInfo.GetTime() % 10 != correctTime)
+            yield return "trycancel";
+          Buttons[2].OnInteract();
+          yield return new WaitForSeconds(0.1f);
+          break;
+
+        case "LOG AT 2":
+          while ((int)BombInfo.GetTime() % 10 == correctTime)
+            yield return "trycancel";  // Fixes a really obscure bug with tp
+          while ((int)BombInfo.GetTime() % 10 != correctTime)
+            yield return "trycancel";
+          Buttons[2].OnInteract();
+          yield return new WaitForSeconds(0.1f);
+          break;
+
+          case "LOG AT 0":
+            while ((int)BombInfo.GetTime() % 10 == correctTime)
+              yield return "trycancel";  // Fixes a really obscure bug with tp
+            while ((int)BombInfo.GetTime() % 10 != correctTime)
+              yield return "trycancel";
+            Buttons[2].OnInteract();
+            yield return new WaitForSeconds(0.1f);
+            break;
+      }
    }
 
-   IEnumerator TwitchHandleForcedSolve () {
-      yield return null;
-   }
+IEnumerator TwitchForceSolveWait() {
+  int twitchTimer = correctTime;
+  string twitchTimerCommand = twitchTimer.ToString();
+  yield return ProcessTwitchCommand("SUBMIT AT " + twitchTimerCommand);
+  yield return new WaitForSeconds(0.1f);
 }
 
-//This is very disgusting code. I am not very good at C#, and this makes it very evident
+   IEnumerator TwitchHandleForcedSolve () {
+     Debug.LogFormat("[Lengthy Lockdown " + "#" + ModuleId + "] Twitch Plays has been force-solved. Submitting time as " + correctTime + ".", ModuleId);
+     yield return new WaitForSeconds(2f);
+     Buttons[0].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[2].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[1].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[0].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[2].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[0].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[2].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[0].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[2].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[1].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[0].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[2].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[1].OnInteract();
+     yield return new WaitForSeconds(0.1f);
+     Buttons[0].OnInteract();
+     StartCoroutine(TwitchForceSolveWait());
+     yield return null;
+}
+}
+
 //Pull up in the Tonka truck!!!
 //Thanks to Jah
--- Assets/Materials/Custom Textures/Materials/ScrollTex.cs
+++ Assets/Materials/Custom Textures/Materials/ScrollTex.cs
@@ -6,11 +6,12 @@ public class ScrollTex : MonoBehaviour {
 
 	public float ScrollX = 0.5f;
 	public float ScrollY = 0.5f;
+	public int spinSpeed = 360;
 
 	// Update is called once per frame
-	void Update () {
+	void Start () {
 		float OffsetX = Time.time * ScrollX;
 		float OffsetY = Time.time * ScrollY;
-		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(OffsetX, OffsetY);
+		 transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
 	}
 }
